0cd8c6a8227f94ee7290f7ba863ac139c4b7363b
rowjoy <rowjoy202@gmail.com>
2025-09-01 15:16:58 +0600

    Development

--- apple-framework/SafariView.swift
+++
@@ -1,8 +0,0 @@
-//
-//  SafariView.swift
-//  apple-framework
-//
-//  Created by Jamirul Islam on 1/9/25.
-//
-
-import Foundation
--- apple-framework/framework.swift
+++
@@ -1,8 +0,0 @@
-//
-//  framework.swift
-//  apple-framework
-//
-//  Created by Jamirul Islam on 31/8/25.
-//
-
-import Foundation
