e93315c03304b1f9a48ef53bc0f278931c2608c3
rowjoy <rowjoy202@gmail.com>
2025-09-11 17:33:26 +0600

    Created Custom TextField Use golobel

---
+++ apple-framework/Screen/Created Account/IconTextField.swift
@@ -0,0 +1,18 @@
+//
+//  IconTextField.swift
+//  apple-framework
+//
+//  Created by Jamirul Islam on 11/9/25.
+//
+
+import SwiftUI
+
+struct IconTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IconTextField()
+}
